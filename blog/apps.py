5c826046d28426093b98fe2ab12406551bc0a02c
duplxey <nick.tomazic@gmail.com>
2021-01-12 22:16:42 +0100

    CQRS -> ORM

--- blog/apps.py
+++ blog/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
 
-from blog.models import Article
-
 
 class BlogConfig(AppConfig):
     name = 'blog'
-
-    def ready(self):
-        # Create the Article table
-        Article.create_table()
