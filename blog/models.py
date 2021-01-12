535deaf71ef605247afdaef3c4991a23f6ef9c76
duplxey <nick.tomazic@gmail.com>
2021-01-12 23:00:18 +0100

    ArticleSchema, get_article, get_article_list endpoints.

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,6 @@ class Article(models.Model):
     author = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=512)
     content = models.TextField()
+
+    def __str__(self):
+        return self.author.username + ": " + self.title
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,31 @@
+from django.http import JsonResponse
+
+from blog.models import Article
+from blog.schemas import ArticleSchema
+
+
 def create_article(request):
     pass
 
 
 def get_article(request, article_id):
-    pass
+    try:
+        article = Article.objects.get(id=article_id)
+        schema = ArticleSchema.from_django(article)
+        return JsonResponse({
+            'article': schema.dict()
+        })
+    except Article.DoesNotExist:
+        return JsonResponse({'detail': 'Cannot find an article with this id.'}, status=404)
 
 
 def get_article_list(request):
-    pass
+    articles = Article.objects.all()
+    data = []
+
+    for article in articles:
+        data.append(ArticleSchema.from_django(article).dict())
+
+    return JsonResponse({
+        'articles': data
+    })
